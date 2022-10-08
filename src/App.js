7944727924416aad67a4d75e4776da68720c38fc
Avighna <gandra.avighna@gmail.com>
2022-10-08 23:19:23 +0530

    frstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react'
 
 function App() {
+ 
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('volunteerList.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+    
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className='table table-hover mx-auto m-3 w-75'>
+        <thead>
+        <tr className="p-2 bg-info">
+          <th>Name</th>
+          <th>Team</th>
+          <th>Mail ID</th>
+        </tr>
+        </thead>
+        <tbody>
+      
+      {
+        data.map((item,ind)=><tr key={ind}><td >{item.Name}</td>
+                                  <td >{item.Team}</td>  
+                                  <td><a href='mailto:${item.Mail}'>{item.Mail}</a></td>
+                            </tr>)
+      }
+      </tbody>
+      </table>
     </div>
   );
 }
